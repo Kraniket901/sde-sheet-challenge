99864314708c173c78440a87e1d604a2d9948b50
Aniket Kumar <kraniket123654@gmail.com>
2023-06-07 14:55:00 +0530

    18

--- uniquePath.cpp
+++ uniquePath.cpp
@@ -31,3 +31,23 @@ int uniquePaths(int n, int m) {
 	}
 	return res;
 }
+
+//uniquepath with restrictions (gfg)
+class Solution {
+    public:
+    int mod = 1e9+7;
+    int func(int i, int j, vector<vector<int>> &grid, vector<vector<int>> &dp){
+        if(i<0 || j<0 || grid[i][j]==0) return 0;
+         if(i==0 && j==0) return 1;
+        if(dp[i][j]!=-1) return dp[i][j];
+        int up = func(i-1, j, grid, dp);
+        int left = func(i, j-1, grid, dp);
+        return dp[i][j] = (up + left)%mod;
+    }
+  public:
+    int uniquePaths(int n, int m, vector<vector<int>> &grid) {
+        // code here
+        vector<vector<int>>dp(n,vector<int>(m,-1));
+        return func(n-1,m-1, grid, dp);
+        
+    }
